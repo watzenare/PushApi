6f4c19cd03d4a1b954889f13a6bbf2fbcb52fabf
watzenare <eloi.balllara@gmail.com>
2014-12-10 14:27:12 +0100

    - DB updated with a new table, this table is used to improve theme names descriptions

--- PushApi/System/pushdb.sql
+++ PushApi/System/pushdb.sql
@@ -5,11 +5,26 @@ USE pushdb;
 CREATE TABLE `users` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `email` varchar(80) NOT NULL,
-  `android_id` int(11) NOT NULL DEFAULT '0',
-  `ios_id` int(11) NOT NULL DEFAULT '0',
+  `android_id` varchar(100) NOT NULL DEFAULT '0',
+  `ios_id` varchar(100) NOT NULL DEFAULT '0',
   `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
   UNIQUE KEY `email` (`email`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8
+
+CREATE TABLE `themes` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(80) NOT NULL,
+  `range` varchar(9) NOT NULL DEFAULT '',
+  `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8
+
+CREATE TABLE `channels` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) NOT NULL,
+  `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8
 
 CREATE TABLE `preferences` (
@@ -25,14 +40,6 @@ CREATE TABLE `preferences` (
   CONSTRAINT `preferences_ibfk_2` FOREIGN KEY (`theme_id`) REFERENCES `themes` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8
 
-CREATE TABLE `themes` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(80) NOT NULL,
-  `range` varchar(9) NOT NULL DEFAULT '',
-  `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8
-
 CREATE TABLE `subscriptions` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `user_id` int(11) NOT NULL,
@@ -43,13 +50,6 @@ CREATE TABLE `subscriptions` (
   KEY `channel_id` (`channel_id`),
   CONSTRAINT `subscriptions_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`),
   CONSTRAINT `subscriptions_ibfk_2` FOREIGN KEY (`channel_id`) REFERENCES `channels` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8
-
-CREATE TABLE `channels` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(100) NOT NULL,
-  `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8
 
 CREATE TABLE `apps` (
@@ -73,8 +73,19 @@ CREATE TABLE `logs` (
   KEY `theme_id` (`theme_id`),
   KEY `channel_id` (`channel_id`),
   KEY `user_id` (`user_id`),
-  CONSTRAINT `logs_ibfk_1` FOREIGN KEY (`channel_id`) REFERENCES `channels` (`id`),
-  CONSTRAINT `logs_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
+  CONSTRAINT `logs_ibfk_1` FOREIGN KEY (`theme_id`) REFERENCES `themes` (`id`),
+  CONSTRAINT `logs_ibfk_2` FOREIGN KEY (`channel_id`) REFERENCES `channels` (`id`),
+  CONSTRAINT `logs_ibfk_3` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8
+
+CREATE TABLE `subjects` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `theme_id` int(11) NOT NULL,
+  `description` varchar(120) NOT NULL DEFAULT '0',
+  `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  KEY `theme_id` (`theme_id`),
+  CONSTRAINT `subjects_ibfk_1` FOREIGN KEY (`theme_id`) REFERENCES `themes` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8
 
 #Show create table subscriptions
